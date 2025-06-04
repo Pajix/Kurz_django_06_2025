58fd0c6f5c8f7bd9f841f08350166a262ae21832
PavelMarecek <polhistorie@gmail.com>
2025-06-04 09:40:58 +0200

    Dokoncena lekce 145 - formular - upravujeme vychozi nastaveni policka

--- funnypage/jokesapp/forms.py
+++ funnypage/jokesapp/forms.py
@@ -2,7 +2,9 @@ from django import forms
 
 
 class JokeForm(forms.Form):
-    user_name = forms.CharField(label="Váše jméno", max_length=100)
+    user_name = forms.CharField(label="Váše jméno", max_length=10,
+                                error_messages={"required": "Jméno nesmí být prázdné.",
+                                                "max_length": "Prosím, vložte kratší jméno."})
 
 
 
