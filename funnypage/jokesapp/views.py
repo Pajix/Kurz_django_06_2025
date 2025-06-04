5d97a679b8fe1daae43c467a075e1031554c56d1
PavelMarecek <polhistorie@gmail.com>
2025-06-04 08:52:00 +0200

    Dokoncena lekce 142 - formular - jednoducha validace formulare

--- funnypage/jokesapp/views.py
+++ funnypage/jokesapp/views.py
@@ -7,7 +7,14 @@ def jokes(request):
     # podmínka pokud se odešle formulář, tak se zobrazi stranka s dekujem
     if request.method == "POST":
         visiter_name = request.POST["username"]
-        print(visiter_name)  # pro testování jestli se nám vrací data
+        # pokud se zadal prazdny string
+        if visiter_name == "":
+            # pokud se zadal prazdny string, tak se zobrazi chyba
+            # v html strance pal vložit {% if empty_username %}
+            return render(request, "jokesapp/index.html", {"empty_username": True})
+
+        print(visiter_name)
+        # pro testování jestli se nám vrací data
         return HttpResponseRedirect("/thank-you")
 
     # jinak se zobrazi stranka s formularem = původní načtení stránky
