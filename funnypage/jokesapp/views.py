5c552171cf70e41ca6e6695403a85390d5be7df0
PavelMarecek <polhistorie@gmail.com>
2025-06-04 08:07:59 +0200

    Dokoncena lekce 140 - formular - odeslani dat na jinou stranku,  dekovaci stranka

--- funnypage/jokesapp/views.py
+++ funnypage/jokesapp/views.py
@@ -3,5 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def jokes(request):
+    # podmínka pokud se odešle formulář, tak se zobrazi stranka s dekujem
+    if request.method == "POST":
+        visiter_name = request.POST["username"]
+        print(visiter_name)  # pro testování jestli se nám vrací data
+        return render(request, "jokesapp/thank-you.html")
+
+    # jinak se zobrazi stranka s formularem
     return render(request, "jokesapp/index.html")
 
