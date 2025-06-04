dad14b1a749928c9d1223066c024718d475668d4
PavelMarecek <polhistorie@gmail.com>
2025-06-04 12:49:34 +0200

    Dokoncena lekce 152 - formular - ukladame data z formulare do databaze

--- funnypage/jokesapp/views.py
+++ funnypage/jokesapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import JokeForm  # import z forms.py který jsme si vytvorili a je tam template pro formular
+from .models import Jokes
 
 
 # Create your views here.
@@ -9,8 +10,12 @@ def jokes(request):
     if request.method == "POST":
         form = JokeForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
-            # pro testování jestli se nám vrací data
+            joke = Jokes(
+                user_name=form.cleaned_data["user_name"],
+                joke_text=form.cleaned_data["joke_text"],
+                rating=form.cleaned_data["rating"],
+            )
+            joke.save()
             return HttpResponseRedirect("/thank-you")
 
     form = JokeForm()
