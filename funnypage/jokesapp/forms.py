b7eff0e00a5774434dffa5da298ee55559565f12
PavelMarecek <polhistorie@gmail.com>
2025-06-04 12:03:56 +0200

    Dokoncena lekce 148 - formular - rozsireni formulare

--- funnypage/jokesapp/forms.py
+++ funnypage/jokesapp/forms.py
@@ -2,9 +2,23 @@ from django import forms
 
 
 class JokeForm(forms.Form):
-    user_name = forms.CharField(label="Váše jméno", max_length=10,
+    user_name = forms.CharField(label="Váše jméno",
+                                max_length=10,
                                 error_messages={"required": "Jméno nesmí být prázdné.",
-                                                "max_length": "Prosím, vložte kratší jméno."})
+                                                "max_length": "Prosím, vložte kratší jméno."},
+                                widget=forms.TextInput(attrs={"placeholder": "Jméno"})
+                                )
+
+    joke_text = forms.CharField(label="Váš vtip",
+                                widget=forms.Textarea(attrs={"placeholder": "Vtip"}),
+                                max_length=250
+                                )
+
+    rating = forms.IntegerField(label="Váš rating",
+                                widget=forms.NumberInput(attrs={"placeholder": "Rating"}),
+                                min_value=1,
+                                max_value=5,
+                                )
 
 
 
